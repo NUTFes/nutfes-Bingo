b3043cda4a63fd60346ac5c3b6c902e521612ceb
TkymHrt <s231053@stn.nagaokaut.ac.jp>
2023-06-19 18:33:39 +0900

    [add] docker-compose.yml の修正＆サンプルの作成

--- view-user/src/pages/index.tsx
+++ view-user/src/pages/index.tsx
@@ -13,8 +13,136 @@ const Page: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
-      <div>
-        <p className={styles.text}>Hello World</p>
+      <div className={styles.container}>
+        <div className={styles.card_frame}>
+          <div className={styles.card}>
+            <div className={`${styles.card_content} ${styles._01}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._02}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._03}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._04}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._05}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._06}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._07}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._08}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._09}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._10}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._11}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._12}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._13}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._14}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._15}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._16}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._17}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._18}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._19}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._20}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._21}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._22}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._23}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._24}`}>
+              <p>
+                null
+              </p>
+            </div>
+            <div className={`${styles.card_content} ${styles._25}`}>
+              <p>
+                null
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
