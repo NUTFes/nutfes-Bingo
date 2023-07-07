1c3afd29b16af76b6336fdc57887cb15476e62e0
TkymHrt <s231053@stn.nagaokaut.ac.jp>
2023-07-08 01:45:34 +0900

    [add] Headerの追加

--- view-user/src/pages/index.tsx
+++ view-user/src/pages/index.tsx
@@ -1,151 +1,24 @@
-import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
+import Head from "next/head";
 import type { NextPage } from "next";
-
+import { Header } from "@/components/common";
 
 const Page: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Bingo</title>
-        <meta name="description" content="NUTFES BINGO" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
-      <div className={styles.container}>
-        <div className={styles.card_frame}>
-          <div className={styles.card}>
-            <div className={`${styles.card_content} ${styles._01}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._02}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._03}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._04}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._05}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._06}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._07}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._08}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._09}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._10}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._11}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._12}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._13}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._14}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._15}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._16}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._17}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._18}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._19}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._20}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._21}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._22}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._23}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._24}`}>
-              <p>
-                null
-              </p>
-            </div>
-            <div className={`${styles.card_content} ${styles._25}`}>
-              <p>
-                null
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+  <div className={styles.container}>
+    <Head>
+      <title>Bingo</title>
+      <meta name="description" content="NUTFES BINGO" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.svg" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&display=swap" rel="stylesheet"></link>
+    </Head>
+    <div>
+      <Header user="USER" />
     </div>
-  );
+  </div>
+  )
 };
 
 export default Page;
