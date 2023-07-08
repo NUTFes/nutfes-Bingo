0ec8bb49fa505f1a00a6cf19f160facf7e5212a8
TkymHrt <s231053@stn.nagaokaut.ac.jp>
2023-07-08 23:51:56 +0900

    [fix] レビュー内容の修正

--- view-user/src/pages/_document.tsx
+++ view-user/src/pages/_document.tsx
@@ -2,8 +2,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja-JP">
+      <Head>
+        <title>Bingo</title>
+        <meta name="description" content="NUTFES BINGO" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&display=swap" rel="stylesheet"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- view-user/src/pages/index.tsx
+++ view-user/src/pages/index.tsx
@@ -7,13 +7,6 @@ import { Header } from "@/components/common";
 const Page: NextPage = () => {
   return (
   <div className={styles.container}>
-    <Head>
-      <title>Bingo</title>
-      <meta name="description" content="NUTFES BINGO" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.svg" />
-      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700&display=swap" rel="stylesheet"></link>
-    </Head>
     <div>
       <Header user="USER" />
     </div>
