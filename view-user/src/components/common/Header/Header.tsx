df836ea1472046d84e063ba1cc1a6b5c5b0f59b9
TkymHrt <131145590+TkymHrt@users.noreply.github.com>
2023-07-15 22:21:59 +0900

    [add] Create a modal to display results

--- view-user/src/components/common/Header/Header.tsx
+++ view-user/src/components/common/Header/Header.tsx
@@ -2,20 +2,30 @@ import React from "react";
 import { ReactNode } from "react";
 import styles from "./Header.module.css";
 import LogoutButton from "@/components/common/LogoutButton";
+import { Modal } from "@/components/common";
 
 interface HeaderProps {
   user: string;
 }
 
-export const Header = (props: HeaderProps) => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.title}>
-        <p>NUTFES BINGO {props.user}</p>
-        <LogoutButton />
-      </div>
-    </div>
-  );
-};
+const Header = (props: HeaderProps) =>  {
+    return (
+        <div className={styles.container}>
+            <div className={styles.main}>
+                <div className={styles.title}>
+                    <p>NUTFES BINGO {props.user}</p>
+                </div>
+                <div>
+                    <Modal buttonText="最新の番号を表示">
+                        <div className={styles.modalContent}>
+                            抽選された番号
+                            <p>25</p>
+                        </div>
+                    </Modal>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default Header;
---
+++ view-user/src/components/common/Modal/index.ts
@@ -0,0 +1,2 @@
+// "Modal"ディレクトリ内のモジュール"Modal.tsx"をdefaultとして再エクスポートする。
+export {default} from "./Modal"
