dc47e8a74b37541aeb20479fe5cdb305727e50b3
YosukeIida <stuiida2002@gmail.com>
2023-07-18 20:21:30 +0900

    [add]抽選済み番号一覧コンポーネントの作成

--- view-user/src/pages/index.tsx
+++ view-user/src/pages/index.tsx
@@ -3,12 +3,18 @@ import styles from "@/styles/Home.module.css";
 import Head from "next/head";
 import type { NextPage } from "next";
 import { Header } from "@/components/common";
+import { BingoResult } from "@/components/common";
 
 const Page: NextPage = () => {
+  const dataArray:number[] = [21, 5, 33333]
   return (
   <div className={styles.container}>
     <div>
       <Header user="USER" />
+    </div>
+    <div>
+      {/* 数字は適当 */}
+      <BingoResult dataArray={dataArray}/>
     </div>
   </div>
   )
