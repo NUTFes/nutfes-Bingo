0c46b139b1798687f3f0f36953119b1dcf84a1b2
YosukeIida <stuiida2002@gmail.com>
2023-07-27 10:46:51 +0900

    [fix] カードサイズ，フォントの微調整

--- view-user/src/pages/index.tsx
+++ view-user/src/pages/index.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import Head from "next/head";
 import type { NextPage } from "next";
-import { Header } from "@/components/common";
-import { BingoResult } from "@/components/common";
+import { Header, BingoResult } from "@/components/common";
 
 const Page: NextPage = () => {
-  const bingoResultNumber: number[] = [21, 5, 33, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4 ,55 ,66 ,32];
+  const bingoResultNumber: number[] = [
+    21, 5, 33, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 55,
+    66, 32,
+  ];
   return (
     <div className={styles.container}>
       <div>
