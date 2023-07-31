d538073ce3064d816327f6e170785443480dbeb8
hikahana <22.h.hanada.nutfes@gmail.com>
2023-07-31 06:53:12 +0000

    [fix] spanタグの削除、使わないpropsの削除

--- view-user/src/components/common/Button/Button.tsx
+++ view-user/src/components/common/Button/Button.tsx
@@ -2,25 +2,18 @@ import styles from "./Button.module.css";
 import { CgLogOut } from "react-icons/cg";
 import classNames from "classnames";
 
-
-const Button = ({
-  theme = "primary",
-  size = "l",
-  shape = "circle",
-  ...props
-}) => {
-
+const Button = ({ theme = "primary", size = "l", shape = "circle" }) => {
   return (
     <main>
       <div>
         <button
-          className={classNames(styles[theme],styles[size],styles[shape])}
+          className={classNames(styles[theme], styles[size], styles[shape])}
           onClick={() => console.log("button click")}
         >
-          <span className={styles.contents}>
+          <div className={styles.contents}>
             <CgLogOut className={styles.icon} />
             <p>Logout</p>
-          </span>
+          </div>
         </button>
       </div>
     </main>
