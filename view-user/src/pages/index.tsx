926f6616bc2ad41ef1b2863d2e46a10151e11daa
hikahana <22.h.hanada.nutfes@gmail.com>
2023-08-02 04:42:13 +0000

    [mod] indexから投げるボタンを投げるように変更

--- view-user/src/pages/index.tsx
+++ view-user/src/pages/index.tsx
@@ -2,18 +2,23 @@ import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import Head from "next/head";
 import type { NextPage } from "next";
-import { Header,Button } from "@/components/common";
-
+import { Header, Button } from "@/components/common";
+import { CgLogOut } from "react-icons/cg";
 
 const Page: NextPage = () => {
   return (
-  <div className={styles.container}>
-    <div>
-      <Header user="USER" />
+    <div className={styles.container}>
+      <div>
+        <Header user="USER" />
+      </div>
+      <Button size="l" shape="circle">
+        <div className={styles.contents}>
+          <CgLogOut className={styles.icon} />
+          <p>Logout</p>
+        </div>
+      </Button>
     </div>
-    <Button/>
-  </div>
-  )
+  );
 };
 
 export default Page;
