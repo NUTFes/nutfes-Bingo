fd71a1b8144a15fb0bddc3bac6d734e2325e187f
TkymHrt <131145590+TkymHrt@users.noreply.github.com>
2023-08-03 15:32:53 +0900

    Revert "Revert "Merge remote-tracking branch 'origin/feature/issue27/deploy-setting' into fix/issue29/Fix-Modal-Compornent""
    This reverts commit 98a1468c09df7107b3ba9ff7852df2253495794c.

--- view-user/src/components/common/index.ts
+++ view-user/src/components/common/index.ts
@@ -4,3 +4,4 @@
 export {default as Header} from "./Header"
 export {default as Modal} from "./Modal"
 export {default as Button} from "./Button"
+export {default as BingoResult} from "./BingoResult"
