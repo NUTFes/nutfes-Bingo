d2802ed7e3273c631a3fadf0ad58d663dcb0d57d
TkymHrt <131145590+TkymHrt@users.noreply.github.com>
2023-08-06 11:24:08 +0900

    [fix] 番号削除の仕組みを変更
    input要素に入力すると、配列内に指定の番号が存在する場合には、select要素にも指定の数字が表示される。またselect要素で選択した数字はinput要素にも反映され表示される。

--- view-admin/src/pages/index.tsx
+++ view-admin/src/pages/index.tsx
@@ -2,44 +2,109 @@ import styles from "@/styles/Home.module.css";
 import type { NextPage } from "next";
 import { Header, BingoResult, Button } from "@/components/common";
 import { CgLogOut } from "react-icons/cg";
+import { useState } from "react";
 
 const Page: NextPage = () => {
   const bingoResultNumber: number[] = [
     21, 5, 33, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 55,
     66, 32,
   ];
+  const [data, setData] = useState("");
+  const [inputNumber, setInputNumber] = useState<number | null>(null);
+  const [selectedNumber, setSelectedNumber] = useState<number | null>(null);
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
+    const newValue = parseInt(event.target.value);
+    setSelectedNumber(isNaN(newValue) ? null : newValue);
+  };
+
+  const handleSubmit = () => {
+    if (inputNumber !== null) {
+      console.log(inputNumber);
+    } else if (selectedNumber !== null) {
+      console.log(selectedNumber);
+    }
+    setInputNumber(null);
+    setSelectedNumber(null);
+  };
 
   return (
     <div className={styles.container}>
       <Header user="Admin">
         <div className={styles.main}>
           <Button size="l" shape="circle">
-            <div className={styles.LoginButtonContents}>
-              <CgLogOut className={styles.buttonIcon} />
-              <p>Logout</p>
-            </div>
+            <CgLogOut className={styles.buttonIcon} />
+            <p>Logout</p>
           </Button>
         </div>
       </Header>
       <div className={styles.form}>
-        <div className={styles.inputFrame}>
-          <p>抽選した番号を入力</p>
+        <div className={styles.frame}>
+          <p className={styles.centerText}>抽選した番号を入力</p>
           <form className={styles.item}>
-            <input type="text" name="name" placeholder="番号を入力" className={styles.inputForm} />
-            <button type="submit" className={styles.inputButton}>送信</button>
+            <input
+              type="number"
+              min="0"
+              max="99"
+              name="data"
+              placeholder="番号を入力"
+              value={data}
+              onChange={(event) => setData(event.target.value)}
+              className={styles.inputForm}
+            />
+            <button
+              type="button"
+              className={styles.Button}
+              onClick={() => {
+                console.log(data);
+                setData("");
+              }}
+            >
+              送信
+            </button>
           </form>
         </div>
-        <div className={styles.deleteFrame}>
-          <p>抽選した番号を削除</p>
+        <div className={styles.frame}>
+          <p className={styles.centerText}>抽選した番号を削除</p>
           <form className={styles.item}>
-            <input type="text" name="name" placeholder="番号を入力" className={styles.inputForm} />
-            <select>
-              <option value="" hidden>選択してください</option>
+            <input
+              type="number"
+              min="0"
+              max="99"
+              name="data"
+              placeholder="番号を入力"
+              value={selectedNumber !== null ? selectedNumber : ""}
+              onChange={handleSelectChange}
+              className={styles.inputForm}
+            />
+            <select
+              value={
+                inputNumber !== null
+                  ? ""
+                  : selectedNumber !== null
+                  ? selectedNumber
+                  : ""
+              }
+              onChange={handleSelectChange}
+              disabled={inputNumber !== null}
+            >
+              <option value="" hidden>
+                選択してください
+              </option>
               {bingoResultNumber.map((number, index) => (
-                <option key={index} value={number}>{number}</option>
+                <option key={index} value={number}>
+                  {number}
+                </option>
               ))}
             </select>
-            <button type="submit" className={styles.deleteButton}>送信</button>
+            <button
+              type="button"
+              className={styles.Button}
+              onClick={handleSubmit}
+              disabled={inputNumber === null && selectedNumber === null}
+            >
+              送信
+            </button>
           </form>
         </div>
       </div>
@@ -49,4 +114,3 @@ const Page: NextPage = () => {
 };
 
 export default Page;
-
