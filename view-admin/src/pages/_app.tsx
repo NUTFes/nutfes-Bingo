59223ef94bd2cbe666377dc4ae33cfdd68b8d2aa
nose221834 <22.t.ichinose.nutfes@gmail.com>
2023-08-06 18:18:14 +0900

    keycloakのログイン機能

--- view-admin/src/pages/_app.tsx
+++ view-admin/src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
+  router,
 }: AppProps) {
+  useEffect(() => {
+    if (!session) {
+      router.push("/");
+    }
+  }, [router, session]);
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
