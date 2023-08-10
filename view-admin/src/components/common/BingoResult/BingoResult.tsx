d95f179a87bff00159d8edcfd8c7089d48d112c0
YosukeIida <stuiida2002@gmail.com>
2023-08-11 04:02:54 +0900

    [fix] 番号一覧を逆順にする処理の変更

--- view-admin/src/components/common/BingoResult/BingoResult.tsx
+++ view-admin/src/components/common/BingoResult/BingoResult.tsx
@@ -13,7 +13,7 @@ export const BingoResult = (props: BingoResultProps) => {
       <div className={styles.container}>
         <div className={styles.frame_title}>抽選済み番号一覧</div>
         <div className={styles.card_frame}>
-            {props.bingoResultNumber.reverse().map((num, index) => (
+            {[...props.bingoResultNumber].reverse().map((num, index) => (
             <div className={styles.card} key={index}>
               <div className={styles.card_content}> {num.data}</div>
             </div>
