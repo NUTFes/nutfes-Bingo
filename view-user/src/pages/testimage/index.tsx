4fd52e11cbf7eae8e68934b51a254d20c97d1262
YosukeIida <stuiida2002@gmail.com>
2023-08-20 16:15:48 +0900

    wip

---
+++ view-user/src/pages/testimage/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import styles from "@/styles/Home.module.css";
+import {
+
+} from "@/components/common";
+
+const Page: NextPage = () => {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  );
+};
+
+export default Page;
