3e19d42a574285b57c83890c763a3bcf96c71417
hikahana <22.h.hanada.nutfes@gmail.com>
2023-09-01 16:23:08 +0000

    [mod] reacthookformでバリデーションつけた

--- view-admin/src/pages/index.tsx
+++ view-admin/src/pages/index.tsx
@@ -19,45 +19,40 @@ import {
 } from "@/utils/api_methods";
 
 interface formData {
-  submitBingoNumber: number;
+  submitNumber: number;
+  inputedNumber: number;
+  selectedNumber: number;
 }
 
 const Page: NextPage = () => {
   const { data: session } = useSession();
   const router = useRouter();
   const [bingoNumbers, setBingoNumbers] = useState<BingoNumber[]>([]);
-  const [data, setData] = useState<number | null>(null);
-  const [inputNumber, setInputNumber] = useState<number | null>(null);
-  const [selectedNumber, setSelectedNumber] = useState<number | null>(null);
+  const [isOpened, setIsOpened] = useState<boolean>(false);
+  const isopenBool = () => setIsOpened(!isOpened);
 
   const {
     register,
     handleSubmit,
+    getValues,
     reset,
-    formState: {errors},
+    formState: { errors },
   } = useForm<formData>();
 
-  const onSubmit = (data: formData) => {
-    console.log("onSubmit:", data.submitBingoNumber);
-    createMethod(data.submitBingoNumber);
-    setData(null);
+  const handleSubmitCreate: SubmitHandler<formData> = (data) => {
+    createMethod(data.submitNumber);
   };
 
-  const logoutClick = () => {
-    signOut({ callbackUrl: "/" });
-  };
-
-  const [inputNumbers, setInputNumbers] = useState<number[]>([]);
-  const [isIncluded, setIsIncluded] = useState(false);
-
-  const checkInclusion = () => {
-    if (inputNumbers.every(number => bingoNumbers.map(num => num.data).includes(number))) {
-      setIsIncluded(true);
+  const handleSubmitDelete = async () => {
+    const { inputedNumber, selectedNumber } = getValues();
+    if (inputedNumber) {
+      deleteMethod(inputedNumber);
+      reset({ inputedNumber: 0 });
+    } else if (selectedNumber) {
+      deleteMethod(selectedNumber);
+      reset({ selectedNumber: 0 });
     }
   };
-
-  const [isOpened, setIsOpened] = useState(false);
-  const isopenBool = () => setIsOpened(!isOpened);
 
   useEffect(() => {
     async function fetchBingoNumbers() {
@@ -70,70 +65,46 @@ const Page: NextPage = () => {
         console.error("データの取得中にエラーが発生しました:", error);
       }
     }
-
     fetchBingoNumbers();
   }, [bingoNumbers]);
 
-  const handleSelectChange = (
-    event:
-      | React.ChangeEvent<HTMLInputElement>
-      | React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const newValue = parseInt(event.target.value);
-    setSelectedNumber(isNaN(newValue) ? null : newValue);
-  };
-
-  async function createMethod(data: number | null) {
-    if ((data !== null)) {
-      const newBingoNumber = await createBingoNumber(data);
-      if (newBingoNumber) {
-        console.log("Bingo number created:", newBingoNumber);
-      } else {
-        console.error("Failed to create bingo number.");
-      }
+  async function createMethod(data: number) {
+    const newBingoNumber = await createBingoNumber(data);
+    if (newBingoNumber) {
+      console.log("Bingo number created:", newBingoNumber);
     } else {
-      console.log("data is null");
+      console.error("Failed to create bingo number.");
     }
   }
 
   async function deleteMethod(data: number) {
-    const BingoNumber = await deleteBingoNumber(data);
-    if (BingoNumber) {
-      console.log("Bingo number deleted:", BingoNumber);
+    const deletedBingoNumber = await deleteBingoNumber(data);
+    reset();
+    if (deletedBingoNumber) {
+      console.log("Bingo number deleted:", deletedBingoNumber);
     } else {
-      console.error("Failed to delete bingo number.", Error);
+      console.error("Failed to delete bingo number.");
     }
   }
-
-  const handleSubmitDelete = async () => {
-    if (inputNumber !== null) {
-      console.log(inputNumber);
-      deleteMethod(inputNumber);
-    } else if (selectedNumber !== null) {
-      console.log(selectedNumber);
-      deleteMethod(selectedNumber);
-    }
-    setInputNumber(null);
-    setSelectedNumber(null);
-  };
 
   if (session) {
     return (
       <div className={styles.container}>
-        <JudgementModal
-        isOpened={isOpened}
-        isIncluded={isIncluded}
-        setIsOpened={setIsOpened}
-        setIsIncluded={setIsIncluded}
-        setInputNumbers={setInputNumbers}
-        checkInclusion={checkInclusion}
-        />
+        <JudgementModal isOpened={isOpened} setIsOpened={setIsOpened} />
         <Header user="Admin">
           <div className={styles.main}>
-            <button type="button" onClick={isopenBool} className={styles.btnOpen}>
+            <button
+              type="button"
+              onClick={isopenBool}
+              className={styles.btnOpen}
+            >
               ビンゴ正誤判定
             </button>
-            <Button size="m" shape="circle" onClick={logoutClick}>
+            <Button
+              size="m"
+              shape="circle"
+              onClick={() => signOut({ callbackUrl: "/" })}
+            >
               <CgLogOut className={styles.buttonIcon} />
               <p>Logout</p>
             </Button>
@@ -142,63 +113,53 @@ const Page: NextPage = () => {
         <div className={styles.form}>
           <div className={styles.frame}>
             <p>抽選した番号を入力</p>
-
-            <form className={styles.item} onSubmit={handleSubmit(onSubmit)}>
-              <input
-                {...register('submitBingoNumber', {
-                  required: true,
-                  max: { value: 99, message: '99以下にしてください'},
-                  min: {value: 0, message: '1以上にしてください'},
-                })}
-                type="number" 
-                min="0"
-                max="99"
-                name="data"
-                placeholder="番号を入力"
-                value={data !== null ? data : ""}
-                onChange={(event) => setData(event.target.valueAsNumber)}
-                className={styles.inputForm}
-              />
-              {errors.submitBingoNumber && (
-                <p>{errors.submitBingoNumber.message} </p>
-              )}
-              <button
-                type="submit"
-                className={styles.Button}
-                onClick={async () => {
-                  console.log(data);
-                  console.log(register)
-                  await createMethod(data);
-                  setData(null);
-                }}
-              >
-                送信
-              </button>
+            <form onSubmit={handleSubmit(handleSubmitCreate)}>
+              <div className={styles.item}>
+                <div className={styles.flexerror}>
+                  <input
+                    {...register("submitNumber", {
+                      max: 99,
+                      min: 1,
+                    })}
+                    type="number"
+                    placeholder="番号を入力"
+                    className={styles.inputForm}
+                  />
+                  {errors.submitNumber && (
+                    <div className={styles.errormessage}>
+                      1~99の番号を入力してください
+                    </div>
+                  )}
+                </div>
+                <button type="submit" className={styles.Button}>
+                  送信
+                </button>
+              </div>
             </form>
           </div>
           <div className={styles.frame}>
             <p className={styles.centerText}>抽選した番号を削除</p>
-            <form className={styles.item}>
-              <input
-                type="number"
-                min="0"
-                max="99"
-                name="data"
-                placeholder="番号を入力"
-                value={selectedNumber !== null ? selectedNumber : ""}
-                onChange={handleSelectChange}
-                className={styles.inputForm}
-              />
+            <div className={styles.item}>
+              <div className={styles.flexerror}>
+                <input
+                  {...register("inputedNumber", {
+                    max: 99,
+                    min: 1,
+                  })}
+                  type="number"
+                  placeholder="番号を入力"
+                  className={styles.inputForm}
+                  onChange={() => reset({ selectedNumber: 0 })}
+                />
+                {(errors.inputedNumber || errors.selectedNumber) && (
+                  <div className={styles.errormessage}>
+                    1~99の番号を入力してください
+                  </div>
+                )}
+              </div>
               <select
-                value={
-                  inputNumber !== null
-                    ? ""
-                    : selectedNumber !== null
-                    ? selectedNumber
-                    : ""
-                }
-                onChange={handleSelectChange}
-                disabled={inputNumber !== null}
+                {...register("selectedNumber")}
+                onChange={() => reset({ inputedNumber: 0 })}
               >
                 <option value="" hidden>
                   選択してください
@@ -213,24 +174,26 @@ const Page: NextPage = () => {
                 type="button"
                 className={styles.Button}
                 onClick={handleSubmitDelete}
-                disabled={inputNumber === null && selectedNumber === null}
               >
                 送信
               </button>
-            </form>
+            </div>
           </div>
         </div>
         <BingoResult bingoResultNumber={bingoNumbers} />
       </div>
     );
   }
+
   return (
     <div className={styles.loginContainer}>
       <Header user="Admin Login">
         <div className={styles.main}></div>
       </Header>
       <div className={styles.loginButton}>
-        <Button size="l" shape="square" onClick={() => signIn()}>Log in</Button>
+        <Button size="l" shape="square" onClick={() => signIn()}>
+          Log in
+        </Button>
       </div>
     </div>
   );
