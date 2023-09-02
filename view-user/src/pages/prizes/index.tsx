43fee63bd0ec5b81f101ad8609692ceb7a8886ca
YosukeIida <stuiida2002@gmail.com>
2023-09-03 07:14:30 +0900

    [fix] user景品一覧ページ

---
+++ view-user/src/pages/prizes/index.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from "next";
+import styles from "./prizes.module.css";
+import { Header, Button, PrizeResult } from "@/components/common";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import {
+  BingoPrize,
+  subscriptionBingoPrize,
+} from "@/utils/api_methods";
+
+const Page: NextPage = () => {
+  const router = useRouter();
+  const [bingoPrize, setBingoPrize] = useState<BingoPrize[]>([]); // get
+
+  useEffect(() => {
+    async function fetchBingoPrizes() {
+      try {
+        const response: BingoPrize[] = await subscriptionBingoPrize();
+        if (response) {
+          setBingoPrize(response);
+        }
+      } catch (error) {
+        console.error("データの取得中にエラーが発生しました:", error);
+      }
+    }
+
+    fetchBingoPrizes();
+  }, [bingoPrize]);
+
+  return (
+    <div className={styles.container}>
+      <Header user="">
+        <div className={styles.main}>
+          <Button size="m" shape="circle" onClick={() => router.push("/")}>
+            <div className={styles.buttonContents}>Number</div>
+          </Button>
+        </div>
+      </Header>
+      <PrizeResult prizeResult={bingoPrize} />
+    </div>
+  );
+};
+
+export default Page;
