81182ed714c566df883788e39dbfbb1374f0ac17
YosukeIida <stuiida2002@gmail.com>
2023-09-02 16:13:56 +0900

    [wip] 途中

--- view-admin/src/components/common/PrizeResult/PrizeResult.tsx
+++ view-admin/src/components/common/PrizeResult/PrizeResult.tsx
@@ -12,22 +12,27 @@ export const PrizeResult = (props: PrizeResultProps) => {
   return (
     <div className={styles.content_wrapper}>
       <div className={styles.container}>
-        <div className={styles.frame_title}>抽選済み番号一覧</div>
-          {[...props.prizeResult].reverse().map((prizeResult, index) => (
-            <div className={styles.card_frame} key={index}>
-              <Image
-                src={prizeResult.image}
-                alt="PrizeImage"
-                width={50}
-                height={50}
-                sizes="100vw"
-              />
-              <p>{prizeResult.name}</p>
-              <p>{prizeResult.existing}</p>
+        <div className={styles.frame_title}>景品一覧</div>
+        <div className={styles.card_frame}>
+          {[...props.prizeResult].map((prizeResult, index) => (
+            <div className={styles.card} key={index}>
+              <div className={styles.card_content}>
+                
+                <Image
+                  src={prizeResult.image}
+                  alt="PrizeImage"
+                  width={50}
+                  height={50}
+                  sizes="100vw"
+                />
+                <p>{prizeResult.name}</p>
+                <p>{prizeResult.existing}</p>
+              </div>
             </div>
           ))}
         </div>
       </div>
+    </div>
   );
 };
 
