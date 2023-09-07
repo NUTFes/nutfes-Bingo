fe5264f36af56005c523a95e0a2041717c3fa000
TkymHrt <131145590+TkymHrt@users.noreply.github.com>
2023-09-07 20:40:13 +0900

    [fix] 管理者ページのオーバーレイを修正

--- view-admin/src/components/common/PrizeResult/PrizeResult.tsx
+++ view-admin/src/components/common/PrizeResult/PrizeResult.tsx
@@ -18,32 +18,49 @@ export const PrizeResult = (props: PrizeResultProps) => {
           {[...props.prizeResult]
             .sort((a, b) => a.id - b.id)
             .map((prizeResult) => (
-              <div className={prizeResult.existing? styles.card_overlay : styles.card} key={prizeResult.id}>
-                <div style={{position:"relative", width:"100%", height:"100%"}}>
+              <div className={styles.card} key={prizeResult.id}>
+                <div
+                  style={{
+                    position: "relative",
+                    width: "100%",
+                    height: "100%",
+                  }}
+                >
                   <Image
                     src={prizeResult.image}
+                    className={styles.image}
                     alt="PrizeImage"
                     fill
+                    style={{ objectFit: "cover" }}
                   />
-                  <p>{prizeResult.existing? "当選" : ""}</p>
                 </div>
-                <div style={{position:"relative"}} className={styles.card_content}>
+                <div
+                  style={{ position: "relative" }}
+                  className={styles.card_content}
+                >
                   {prizeResult.name}
-                  <div className={styles.toggle_button}>
-                  <input
-                    id="toggle"
-                    className={styles.toggle_input}
-                    type="checkbox"
-                    checked={prizeResult.existing}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                      console.log(prizeResult.id, e.target.checked);
-                      updatePrizeExisting(prizeResult.id, e.target.checked);
-                    }}
-                  />
-                  <label htmlFor="toggle" className={styles.toggle_label} />
                 </div>
-              </div>
-             </div>
+                {prizeResult.existing && (
+                  <div className={styles.overlay}>
+                    <p>当選！</p>
+                  </div>
+                )}
+                <div className={styles.toggle_container}>
+                  <div className={styles.toggle_button}>
+                    <input
+                      id="toggle"
+                      className={styles.toggle_input}
+                      type="checkbox"
+                      checked={prizeResult.existing}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                        console.log(prizeResult.id, e.target.checked);
+                        updatePrizeExisting(prizeResult.id, e.target.checked);
+                      }}
+                    />
+                    <label htmlFor="toggle" className={styles.toggle_label} />
+                </div>
+                </div>
+            </div>
             ))}
         </div>
       </div>
