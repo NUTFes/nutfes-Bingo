8345123616abbd801703eff3786ddadcdad66c0a
YosukeIida <stuiida2002@gmail.com>
2023-09-11 03:38:57 +0900

    [mod] 景品追加ページに画像を表示

--- view-admin/src/pages/testimage/index.tsx
+++ view-admin/src/pages/testimage/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import { useState, useCallback, useEffect } from "react";
 import styles from "@/styles/Home.module.css";
-import { BingoPrize, createBingoPrize, postBingoPrize, subscriptionBingoPrize, updatePrizeExisting} from "@/utils/api_methods";
+import { BingoPrize, createBingoPrize, getBingoPrize, postBingoPrize, updatePrizeExisting} from "@/utils/api_methods";
 
 const Page: NextPage = () => {
   // アップロードした画像ファイルから取得したbase64
@@ -14,7 +14,7 @@ const Page: NextPage = () => {
   useEffect(() => {
     async function fetchBingoPrizes() {
       try {
-        const response: BingoPrize[] = await subscriptionBingoPrize();
+        const response: BingoPrize[] = await getBingoPrize();
         if (response) {
           setBingoPrize(response);
         }
@@ -63,9 +63,12 @@ const Page: NextPage = () => {
     console.log(prizeID,prizeExisting);
   };
 
-  const insertPrize = () => {
+  const insertPrize = async () => {
     postBingoPrize(prizeExisting, displayImage, prizeName);
-    console.log(prizeExisting, displayImage, prizeName)
+    // setBingoPrize(await getBingoPrize());
+    setDisplayImage("");
+    setPrizeName("");
+    setPrizeExisting(false);
   }
 
   return (
