062b032bd150d170e851c0a69b5f44a5aec89d1d
TkymHrt <131145590+TkymHrt@users.noreply.github.com>
2023-09-13 18:50:55 +0900

    [add] ヘッダーカラーを追加

--- view-admin/src/pages/_document.tsx
+++ view-admin/src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="ja-JP">
       <Head>
         <title>Bingo Admin</title>
+        <meta name="theme-color" content="#07033eff"></meta>
         <meta name="description" content="NUTFES BINGO Admin" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
