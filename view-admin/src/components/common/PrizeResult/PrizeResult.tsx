dbbc157dde1a14a557ff25bb6e712c3836724fae
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-04-04 16:20:32 +0900

    [feat] トグルとオーバーレイの表示設定を追加

--- view-admin/src/components/common/PrizeResult/PrizeResult.tsx
+++ view-admin/src/components/common/PrizeResult/PrizeResult.tsx
@@ -1,15 +1,19 @@
-import React from "react";
-import { ReactNode, useState } from "react";
+import React, { useState } from "react";
 import styles from "./PrizeResult.module.css";
 import { BingoPrize, updatePrizeExisting } from "@/utils/api_methods";
 import Image from "next/image";
 
-
 interface PrizeResultProps {
   prizeResult: BingoPrize[];
+  showOverlay: boolean;
+  showToggle: boolean;
 }
 
-export const PrizeResult = (props: PrizeResultProps) => {
+export const PrizeResult = ({
+  prizeResult,
+  showOverlay = true,
+  showToggle = true,
+}: PrizeResultProps) => {
   const [isImageVisible, setIsImageVisible] = useState(true);
   const imageVisibility = () => {
     setIsImageVisible(false);
@@ -18,12 +22,19 @@ export const PrizeResult = (props: PrizeResultProps) => {
     <div className={styles.content_wrapper}>
       <div className={styles.container}>
         <div className={styles.frame_title}>景品一覧</div>
-        <div id="loading" className={isImageVisible ? styles.visible : styles.hidden}></div>
+        <div
+          id="loading"
+          className={isImageVisible ? styles.visible : styles.hidden}
+        ></div>
         <div className={styles.card_frame}>
-          {[...props.prizeResult]
+          {[...prizeResult]
             .sort((a, b) => a.id - b.id)
             .map((prizeResult) => (
-              <div className={styles.card} key={prizeResult.id} id={`prize-${prizeResult.id}`}>
+              <div
+                className={styles.card}
+                key={prizeResult.id}
+                id={`prize-${prizeResult.id}`}
+              >
                 <div
                   style={{
                     position: "relative",
@@ -43,27 +54,29 @@ export const PrizeResult = (props: PrizeResultProps) => {
                 <div className={styles.card_content}>
                   <p>{prizeResult.name}</p>
                 </div>
-                {prizeResult.existing && (
+                {showOverlay && prizeResult.existing && (
                   <div className={styles.overlay}>
                     <p>当選！</p>
                   </div>
                 )}
-                <div className={styles.toggle_container}>
-                  <div className={styles.toggle_button}>
-                    <input
-                      id="toggle"
-                      className={styles.toggle_input}
-                      type="checkbox"
-                      checked={prizeResult.existing}
-                      onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                        console.log(prizeResult.id, e.target.checked);
-                        updatePrizeExisting(prizeResult.id, e.target.checked);
-                      }}
-                    />
-                    <label htmlFor="toggle" className={styles.toggle_label} />
-                </div>
-                </div>
-            </div>
+                {showToggle && (
+                  <div className={styles.toggle_container}>
+                    <div className={styles.toggle_button}>
+                      <input
+                        id="toggle"
+                        className={styles.toggle_input}
+                        type="checkbox"
+                        checked={prizeResult.existing}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                          console.log(prizeResult.id, e.target.checked);
+                          updatePrizeExisting(prizeResult.id, e.target.checked);
+                        }}
+                      />
+                      <label htmlFor="toggle" className={styles.toggle_label} />
+                    </div>
+                  </div>
+                )}
+              </div>
             ))}
         </div>
       </div>
