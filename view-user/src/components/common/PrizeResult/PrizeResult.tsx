18f4a4c956dedf5f56ce09c65d873890ee2e03ae
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-07-03 17:24:36 +0900

    [fix] アニメーションの条件を調整

--- view-user/src/components/common/PrizeResult/PrizeResult.tsx
+++ view-user/src/components/common/PrizeResult/PrizeResult.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./PrizeResult.module.css";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -13,10 +13,23 @@ interface PrizeResultProps {
 export const PrizeResult = (props: PrizeResultProps) => {
   const { locale } = useRouter();
   const t = locale === "ja" ? ja : en;
-  const [isImageVisible, setIsImageVisible] = useState(true);
-  const imageVisibility = () => {
-    setIsImageVisible(false);
+  const [isImageVisible, setIsImageVisible] = useState(false);
+  const [loadedImages, setLoadedImages] = useState(0);
+  const totalImages = props.prizeResult.filter((prize) => prize.image).length;
+
+  useEffect(() => {
+    if (
+      (loadedImages === totalImages && totalImages > 0) ||
+      totalImages === 0
+    ) {
+      setIsImageVisible(true);
+    }
+  }, [loadedImages, totalImages]);
+
+  const handleImageLoad = () => {
+    setLoadedImages((prevCount) => prevCount + 1);
   };
+
   return (
     <div className={styles.content_wrapper}>
       <div className={styles.container}>
@@ -24,45 +37,44 @@ export const PrizeResult = (props: PrizeResultProps) => {
           <Image src="/GiftBox.svg" alt="GiftBox" width={19} height={19} />
           {t.SUB_TITLE_PRIZE}
         </div>
-        <div
-          id="loading"
-          className={isImageVisible ? styles.visible : styles.hidden}
-        ></div>
-        <div className={styles.card_frame}>
-          {[...props.prizeResult]
-            .sort((a, b) => a.id - b.id)
-            .map((prizeResult) => (
-              <div className={styles.card} key={prizeResult.id}>
-                <div
-                  style={{
-                    position: "relative",
-                    width: "100%",
-                    height: "100%",
-                  }}
-                >
-                  <Image
-                    src={prizeResult.image}
-                    className={styles.image}
-                    alt="PrizeImage"
-                    fill
-                    style={{ objectFit: "cover" }}
-                    onLoadingComplete={imageVisibility}
-                  />
-                </div>
-                <div
-                  style={{ position: "relative" }}
-                  className={styles.card_content}
-                >
-                  {prizeResult.name}
-                </div>
-                {prizeResult.existing && (
-                  <div className={styles.overlay}>
-                    <p>{t.WINNING_OVERRAY}</p>
+        {!isImageVisible && <div id="loading" className={styles.visible}></div>}
+        {isImageVisible && (
+          <div className={styles.card_frame}>
+            {[...props.prizeResult]
+              .sort((a, b) => a.id - b.id)
+              .map((prizeResult) => (
+                <div className={styles.card} key={prizeResult.id}>
+                  <div
+                    style={{
+                      position: "relative",
+                      width: "100%",
+                      height: "100%",
+                    }}
+                  >
+                    <Image
+                      src={prizeResult.image}
+                      className={styles.image}
+                      alt="PrizeImage"
+                      fill
+                      style={{ objectFit: "cover" }}
+                      onLoadingComplete={handleImageLoad}
+                    />
                   </div>
-                )}
-              </div>
-            ))}
-        </div>
+                  <div
+                    style={{ position: "relative" }}
+                    className={styles.card_content}
+                  >
+                    {prizeResult.name}
+                  </div>
+                  {prizeResult.existing && (
+                    <div className={styles.overlay}>
+                      <p>{t.WINNING_OVERRAY}</p>
+                    </div>
+                  )}
+                </div>
+              ))}
+          </div>
+        )}
       </div>
     </div>
   );
