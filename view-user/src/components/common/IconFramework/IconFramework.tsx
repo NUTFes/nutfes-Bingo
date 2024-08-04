c4c424914a0728838f6995c406033aaa91524b88
hikahana <22.h.hanada.nutfes@gmail.com>
2024-08-04 15:20:37 +0000

    [feat] IconFrameworkの作成

---
+++ view-user/src/components/common/IconFramework/IconFramework.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode, use, useState } from "react";
+import styles from "./IconFramework.module.css";
+import classNames from "classnames";
+
+interface IconFrameworkProps {
+  icon: ReactNode;
+  text: string;
+  outline?: boolean;
+  onClick?: () => void;
+}
+
+const IconFramework = (props: IconFrameworkProps) => {
+  const [colorInversion, setColorInversion] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setColorInversion(!colorInversion);
+    if (props.onClick) {
+      props.onClick();
+    }
+  };
+
+  return (
+    <button
+      className={classNames(styles.iconContainer, {
+        [styles.outline]: props.outline,
+        [styles.color_inversion]: colorInversion,
+      })}
+      onClick={handleClick}
+    >
+      <div className={styles.icon}>{props.icon}</div>
+      <span className={styles.text}>{props.text}</span>
+    </button>
+  );
+};
+
+export default IconFramework;
