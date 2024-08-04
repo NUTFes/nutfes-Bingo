060522f5d5dcb69999b35ba94bc751884a31830c
hikahana <22.h.hanada.nutfes@gmail.com>
2024-08-04 15:21:34 +0000

    コンポーネント確認用ページ。あとで消す

---
+++ view-user/src/pages/test/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import type { NextPage } from "next";
+import { PrizesIcon } from "@/components/common";
+
+const Page: NextPage = () => {
+  return <PrizesIcon />;
+};
+
+export default Page;
