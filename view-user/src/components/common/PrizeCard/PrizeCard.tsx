99dfc09bb3e54dfc01e1c8f3d12dd6498eff58a2
hikahana <22.h.hanada.nutfes@gmail.com>
2024-08-08 05:32:22 +0000

    カードの隙間を削除

--- view-user/src/components/common/PrizeCard/PrizeCard.tsx
+++ view-user/src/components/common/PrizeCard/PrizeCard.tsx
@@ -41,7 +41,6 @@ const PrizeCard = (props: PrizeCardProps) => {
               objectFit: "cover",
               width: "100%",
               height: "100%",
-              borderRadius: "3.125vw",
             }}
           />
         </div>
