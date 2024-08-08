21dcd8deedce661d97eb4544378cdca3e94ac7c8
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-08-08 14:03:37 +0900

    [feat] スタンプ落下の関数化と仮ボタンの追加

--- view-user/src/pages/ScreenPage/index.tsx
+++ view-user/src/pages/ScreenPage/index.tsx
@@ -1,22 +1,53 @@
 import { useEffect, useRef } from "react";
 import Matter from "matter-js";
 
-const images = [
-  "/ReactionIcon/angry.png",
-  "/ReactionIcon/cracker.png",
-  "/ReactionIcon/crap.png",
-  "/ReactionIcon/good.png",
-  "/ReactionIcon/heart.png",
-  "/ReactionIcon/peace.png",
-  "/ReactionIcon/sad.png",
-  "/ReactionIcon/skull.png",
-  "/ReactionIcon/smile.png",
-  "/ReactionIcon/surprise.png",
-];
+const images: { [key: string]: string } = {
+  angry: "/ReactionIcon/angry.png",
+  cracker: "/ReactionIcon/cracker.png",
+  crap: "/ReactionIcon/crap.png",
+  good: "/ReactionIcon/good.png",
+  heart: "/ReactionIcon/heart.png",
+  peace: "/ReactionIcon/peace.png",
+  sad: "/ReactionIcon/sad.png",
+  skull: "/ReactionIcon/skull.png",
+  smile: "/ReactionIcon/smile.png",
+  surprise: "/ReactionIcon/surprise.png",
+};
 
 const ScreenPage = () => {
   const scene = useRef<HTMLDivElement>(null);
   const render = useRef<Matter.Render | null>(null);
+  const engine = useRef<Matter.Engine | null>(null);
+
+  const addCircleById = (key: string) => {
+    if (!images[key]) {
+      console.warn(`Image with ID ${key} not found`);
+      return;
+    }
+
+    const x = Math.random() * window.innerWidth;
+    const image = images[key];
+    const circle = Matter.Bodies.circle(x, 0, 70, {
+      restitution: 0.8,
+      render: {
+        sprite: {
+          texture: image,
+          xScale: 0.2,
+          yScale: 0.2,
+        },
+      },
+    });
+
+    if (engine.current) {
+      Matter.Composite.add(engine.current.world, circle);
+
+      setTimeout(() => {
+        if (engine.current) {
+          Matter.Composite.remove(engine.current.world, circle);
+        }
+      }, 5000);
+    }
+  };
 
   useEffect(() => {
     if (!scene.current) {
@@ -25,11 +56,11 @@ const ScreenPage = () => {
 
     const { Engine, Render, Runner, Bodies, Composite } = Matter;
 
-    const engine = Engine.create();
+    engine.current = Engine.create();
 
     render.current = Render.create({
       element: scene.current,
-      engine: engine,
+      engine: engine.current,
       options: {
         width: window.innerWidth,
         height: window.innerHeight,
@@ -62,43 +93,20 @@ const ScreenPage = () => {
       { isStatic: true, render: { visible: false } },
     );
 
-    Composite.add(engine.world, [ground, leftWall, rightWall]);
+    Composite.add(engine.current.world, [ground, leftWall, rightWall]);
     Render.run(render.current);
 
     const runner = Runner.create();
-    Runner.run(runner, engine);
-
-    const addRandomCircle = () => {
-      const x = Math.random() * window.innerWidth;
-      const image = images[Math.floor(Math.random() * images.length)];
-      const circle = Bodies.circle(x, 0, 70, {
-        restitution: 0.8,
-        render: {
-          sprite: {
-            texture: image,
-            xScale: 0.2,
-            yScale: 0.2,
-          },
-        },
-      });
-
-      Composite.add(engine.world, circle);
-
-      setTimeout(() => {
-        Composite.remove(engine.world, circle);
-      }, 5000);
-    };
-
-    const interval = setInterval(addRandomCircle, 500);
+    Runner.run(runner, engine.current);
 
     return () => {
-      clearInterval(interval);
-
       if (render.current) {
         Matter.Render.stop(render.current);
       }
-      Matter.Composite.clear(engine.world, true);
-      Matter.Engine.clear(engine);
+      if (engine.current) {
+        Matter.Composite.clear(engine.current.world, true);
+        Matter.Engine.clear(engine.current);
+      }
       if (render.current && render.current.canvas) {
         render.current.canvas.remove();
         render.current.textures = {};
@@ -108,8 +116,28 @@ const ScreenPage = () => {
 
   return (
     <div>
+      <section className="button-container">
+        <button onClick={() => addCircleById("angry")}>Angry</button>
+        <button onClick={() => addCircleById("cracker")}>Cracker</button>
+        <button onClick={() => addCircleById("crap")}>Crap</button>
+        <button onClick={() => addCircleById("good")}>Good</button>
+        <button onClick={() => addCircleById("heart")}>Heart</button>
+        <button onClick={() => addCircleById("peace")}>Peace</button>
+        <button onClick={() => addCircleById("sad")}>Sad</button>
+        <button onClick={() => addCircleById("skull")}>Skull</button>
+        <button onClick={() => addCircleById("smile")}>Smile</button>
+        <button onClick={() => addCircleById("surprise")}>Surprise</button>
+      </section>
       <div ref={scene} />
       <style jsx>{`
+        .button-container {
+          position: fixed;
+          top: 10px;
+          left: 10px;
+          display: flex;
+          flex-direction: row;
+          gap: 5px;
+        }
         div {
           display: flex;
           flex-direction: column;
@@ -119,6 +147,9 @@ const ScreenPage = () => {
         }
         canvas {
           border: 1px solid #000;
+        }
+        button {
+          padding: 10px;
         }
       `}</style>
     </div>
