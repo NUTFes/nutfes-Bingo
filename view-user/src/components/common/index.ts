07aa4c7905b2e15d4a45bb8c72f4eba9bf5b6430
hikahana <22.h.hanada.nutfes@gmail.com>
2024-08-10 08:48:48 +0000

    ReactcionsIconコンポーネントの作成

--- view-user/src/components/common/index.ts
+++ view-user/src/components/common/index.ts
@@ -9,6 +9,7 @@ export { default as BingoIcon } from "./BingoIcon";
 export { default as PrizeResult } from "./PrizeResult";
 export { default as IconFramework } from "./IconFramework";
 export { default as PrizesIcon } from "./PrizesIcon";
+export { default as ReactionsIcon } from "./ReactionsIcon";
 export { default as NumberCardSmall } from "./NumberCardSmall";
 export { default as PrizeCard } from "./PrizeCard";
 export { default as NumberCardList } from "./NumberCardList";
