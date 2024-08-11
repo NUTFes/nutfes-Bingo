c2146498293c7826c95a59353dfac15b0b99e2dc
hikahana <22.h.hanada.nutfes@gmail.com>
2024-08-11 11:25:15 +0000

    BackIconの作成

---
+++ view-user/src/components/common/BackIcon/BackIcon.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { IconFramework } from "@/components/common";
+import { TiArrowBack } from "react-icons/ti";
+import { useRouter } from "next/router";
+
+const BackIcon = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (typeof window !== "undefined") {
+      router.push("/");
+    }
+  };
+
+  return (
+    <IconFramework
+      icon={<TiArrowBack />}
+      text="Back"
+      onClick={() => handleClick()}
+    />
+  );
+};
+
+export default BackIcon;
