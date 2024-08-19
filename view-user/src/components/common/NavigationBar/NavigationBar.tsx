4fe006d1d4183a72696e439ab3ff5d752a17e55e
hikahana <22.h.hanada.nutfes@gmail.com>
2024-08-19 18:28:22 +0000

    中央寄せを当てるpropsを追加

--- view-user/src/components/common/NavigationBar/NavigationBar.tsx
+++ view-user/src/components/common/NavigationBar/NavigationBar.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import classNames from "classnames";
 import styles from "./NavigationBar.module.css";
 
 interface NavigationBarProps {
   children: React.ReactNode;
+  isCentered: boolean;
 }
-const NavigationBar = (props: NavigationBarProps) => {
-  return <div className={styles.navigationBar}>{props.children}</div>;
+
+const NavigationBar = ({ children, isCentered }: NavigationBarProps) => {
+  return (
+    <div className={classNames(styles.navigationBar, {
+      [styles.center]: isCentered,
+    })}>
+      {children}
+    </div>
+  );
 };
 
 export default NavigationBar;
