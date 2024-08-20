a4d37223428d6b98c39c94518d3e6f0b9cfaea05
Hayate Akiyama <24.h.akiyama.nutfes@gmail.com>
2024-08-20 16:15:43 +0900

    [feat]エラーが出る部分をコメントアウト

--- view-user/src/components/common/ToggleButton/ToggleButton.tsx
+++ view-user/src/components/common/ToggleButton/ToggleButton.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import styles from "./ToggleButton.module.css";
 
 interface ToggleButtonProps {
