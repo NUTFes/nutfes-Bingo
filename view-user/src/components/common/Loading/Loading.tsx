f7ac8ca05d13bc15abec1f5c4f688db8c3dd2b5c
Hayate Akiyama <24.h.akiyama.nutfes@gmail.com>
2024-08-23 19:55:36 +0900

    [fix]オーバーレイの追加

--- view-user/src/components/common/Loading/Loading.tsx
+++ view-user/src/components/common/Loading/Loading.tsx
@@ -4,10 +4,12 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const Loading = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.main}>
-        <img className={styles.logo} src="./Bingo_logo.png" alt="sample" />
-        <AiOutlineLoading3Quarters className={styles.loader} />
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <div className={styles.main}>
+          <img className={styles.logo} src="./Bingo_logo.png" alt="sample" />
+          <AiOutlineLoading3Quarters className={styles.loader} />
+        </div>
       </div>
     </div>
   );
