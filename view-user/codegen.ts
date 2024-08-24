356e72a824b8344b24cf3f290d22c03a00507598
i2_yosuke <stuiida2002@gmail.com>
2024-08-25 06:36:41 +0900

    [fix] codegen.ts headers部分の修正

--- view-user/codegen.ts
+++ view-user/codegen.ts
@@ -9,8 +9,7 @@ const config: CodegenConfig = {
     {
       "http://api:8080/v1/graphql": {
         headers: {
-          "x-hasura-admin-secret":
-            process.env.HASURA_GRAPHQL_ADMIN_SECRET || "",
+          "x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET,
         },
       },
     },
