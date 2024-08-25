d6fb482801f41fc8e35d4cef0493030613d5c432
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-08-26 03:11:17 +0900

    [feat] 表示切り替え機能実装とコンポーネントの配置を調整

--- view-user/src/components/common/SettingsIcon/SettingsIcon.tsx
+++ view-user/src/components/common/SettingsIcon/SettingsIcon.tsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import { IconFramework } from "@/components/common";
 import { IoIosSettings } from "react-icons/io";
+import { useResultChange } from "@/contexts/ResultChangeContext";
 
-const SettingsIcon = () => {
+interface SettingsIconProps {
+  onClick?: () => void;
+}
+const SettingsIcon = (props: SettingsIconProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { toggleResultChange } = useResultChange();
+
   const handleClick = () => {
     // TODO Modalの開閉を行う
+    toggleResultChange();
   };
 
   return (
--- view-user/src/pages/index.tsx
+++ view-user/src/pages/index.tsx
@@ -1,95 +1,51 @@
-import React, { useEffect, useState } from "react";
 import type { NextPage } from "next";
-import Image from "next/image";
-import styles from "@/styles/Home.module.css";
-import { Header, Modal, BingoResult, Button } from "@/components/common";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import styles from "@/styles/Home.module.css";
+import { Loading, NumberCardLarge, NumberCardList } from "@/components/common";
+import Layout from "@/components/Layout";
 import { ja } from "../locales/ja";
 import { en } from "../locales/en";
-import { MdTranslate } from "react-icons/md";
 import { useSubscription } from "@apollo/client";
 import { SubscribeListNumbersDocument } from "@/type/graphql";
 import type { SubscribeListNumbersSubscription } from "@/type/graphql";
+import { useResultChange } from "@/contexts/ResultChangeContext";
 
 const Page: NextPage = () => {
   const { locale } = useRouter();
   const t = locale === "ja" ? ja : en;
-  const [isOpened, setIsOpened] = useState(true);
-  const router = useRouter();
+  const { resultChange } = useResultChange();
   const [bingoNumbers, setBingoNumbers] = useState<
     SubscribeListNumbersSubscription["numbers"]
   >([]);
-
   const { data } = useSubscription(SubscribeListNumbersDocument);
 
-  //subscriptionを行うためのuseEffect
+  // Subscription handling useEffect
   useEffect(() => {
     if (data) {
       setBingoNumbers(data.numbers);
     }
   }, [data]);
 
-  // 最初のrendrer時だけ実行してモーダルの再表示を防止する。
-  useEffect(() => {
-    const storedIsOpened = localStorage.getItem("isOpened");
-    if (storedIsOpened !== null) {
-      setIsOpened(JSON.parse(storedIsOpened));
-    }
-  }, []);
+  const copiedArray = [...bingoNumbers];
+  const sortCopiedArray = [...bingoNumbers];
+  const firstBingoNumber = copiedArray.pop()?.number ?? 0;
+  const sortFirstBingoNumber =
+    sortCopiedArray.sort((a, b) => a.number - b.number).shift()?.number ?? 0;
 
-  // isOpenedの状態をもとにlocalStorageを更新する。
-  // localStorageにbooleanが保存できないため、json形式に変換して保存する。
-  useEffect(() => {
-    localStorage.setItem("isOpened", JSON.stringify(isOpened));
-  }, [isOpened]);
-
-  // ページのリロード前にlocalStorageを削除する。
-  useEffect(() => {
-    const handleBeforeUnload = () => {
-      localStorage.removeItem("isOpened");
-    };
-
-    window.addEventListener("beforeunload", handleBeforeUnload);
-
-    return () => {
-      window.removeEventListener("beforeunload", handleBeforeUnload);
-    };
-  }, []);
+  const displayBingoNumbers = resultChange
+    ? { large: firstBingoNumber, list: copiedArray.reverse() }
+    : { large: sortFirstBingoNumber, list: sortCopiedArray.slice(1) };
 
   return (
-    <div className={styles.container}>
-      <Modal isOpened={isOpened} setisOpened={setIsOpened}>
-        <div className={styles.languageBlock}>
-          <div className={styles.language}>
-            <p
-              onClick={() => {
-                router.push("/", "/", { locale: "ja" });
-                setIsOpened(false);
-              }}
-            >
-              日本語
-            </p>
-          </div>
-          <div className={styles.language}>
-            <p
-              onClick={() => {
-                router.push("/", "/", { locale: "en" });
-                setIsOpened(false);
-              }}
-            >
-              English
-            </p>
-          </div>
+    <>
+      <Layout pageName={""}>
+        <div className={styles.numberCardLarge}>
+          <NumberCardLarge bingoNumber={displayBingoNumbers.large} />
+          <NumberCardList bingoNumber={displayBingoNumbers.list} />
         </div>
-      </Modal>
-      <Header />
-      <BingoResult bingoResultNumber={bingoNumbers} />
-      <Button size="null" shape="null" onClick={() => setIsOpened(true)}>
-        <div className={styles.iconButton}>
-          <MdTranslate size={35} />
-        </div>
-      </Button>
-    </div>
+      </Layout>
+    </>
   );
 };
 
