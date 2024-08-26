e2dac837badc59b973ad856c796b897592569571
hikahana <22.h.hanada.nutfes@gmail.com>
2024-08-26 17:32:19 +0000

    テスト

--- view-user/src/pages/test/index.tsx
+++ view-user/src/pages/test/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   NumberCardList,
   ReactionStampModal,
@@ -9,34 +9,44 @@ import {
   NavigationBar,
   PrizesIcon,
   ReachCount,
+  Button,
 } from "@/components/common";
 import Layout from "@/components/Layout";
-import { BingoNumber, BingoPrize, PrizeImage } from "@/type/common";
+import { useMutation, useSubscription } from "@apollo/client";
+import {
+  SubscriotionStampTriggersDocument,
+  UpdateOneTriggerFlagDocument,
+} from "@/type/graphql";
+import type {
+  SubscriotionStampTriggersSubscription,
+  UpdateOneTriggerFlagMutation,
+  UpdateOneTriggerFlagMutationVariables,
+} from "@/type/graphql";
 import { useRouter } from "next/router";
 
-const testBingoNumbers: BingoNumber[] = [
-  { id: 1, number: 1, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 2, number: 2, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 3, number: 3, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 4, number: 4, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 5, number: 5, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 6, number: 6, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 7, number: 7, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 8, number: 8, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 9, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 10, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 11, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 12, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 13, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 14, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 15, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 16, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 17, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 18, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 19, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 20, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-  { id: 21, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
-];
+// const testBingoNumbers: BingoNumber[] = [
+//   { id: 1, number: 1, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 2, number: 2, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 3, number: 3, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 4, number: 4, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 5, number: 5, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 6, number: 6, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 7, number: 7, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 8, number: 8, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 9, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 10, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 11, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 12, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 13, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 14, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 15, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 16, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 17, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 18, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 19, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 20, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+//   { id: 21, number: 9, createdAt: "2024-08-01", updatedAt: "2024-08-01" },
+// ];
 
 const testNumber: number = 8;
 
@@ -44,8 +54,22 @@ const HomePage: React.FC = () => {
   const router = useRouter();
   const pageName = router.pathname;
 
+  const [updateFlag] = useMutation<
+    UpdateOneTriggerFlagMutation,
+    UpdateOneTriggerFlagMutationVariables
+  >(UpdateOneTriggerFlagDocument);
+  const [isFlag, setIsFlag] = useState<boolean>(false);
+
+  useEffect(() => {
+    updateFlag({ variables: { id: 2, trigger: isFlag } });
+  }, [isFlag]); // isFlagが変更されたときに実行される
+
+  const handleClick = () => {
+    setIsFlag(!isFlag);
+  };
   return (
     <div>
+      {/* <Button onClick={handleClick}>hello</Button> */}
       {/* <NumberCardSmall BingoNumber={testBingoNumber}></NumberCardSmall> */}
       {/* <PrizeCard BingoPrize={testBingoPrize}></PrizeCard> */}
       {/* <button onClick={toggleModal}>モーダルボタン</button>
@@ -53,11 +77,7 @@ const HomePage: React.FC = () => {
         <ReactionStampModal position={testPosition} images={images} />
       )} */}
       {/* <ReachIcon /> */}
-      {/* <PrizeCardList BingoPrize={testBingoPrizes} /> */}
-      <Layout pageName={pageName}>
-        <NumberCardList firstNumber bingoNumber={testBingoNumbers} />
-      </Layout>
-      {/* <ReachCount count={testNumber} /> */}
+      <Layout pageName="prizes">hello</Layout>
     </div>
   );
 };
