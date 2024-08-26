944ad3f392e3ade36fbd546e33dfe5c35ec149f1
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-08-27 04:02:42 +0900

    [feat] SettingsIconにモーダル開閉機能を実装

--- view-user/src/components/common/SettingsIcon/SettingsIcon.tsx
+++ view-user/src/components/common/SettingsIcon/SettingsIcon.tsx
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 import { IconFramework } from "@/components/common";
 import { IoIosSettings } from "react-icons/io";
+import { useEffect } from "react";
 
 interface SettingsIconProps {
   onClick?: () => void;
+  isOpen: boolean;
+  setIsSettingsModalOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const SettingsIcon = (props: SettingsIconProps) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [colorInversion, setColorInversion] = useState<boolean>(false);
 
   const handleClick = () => {
     if (props.onClick) {
       props.onClick();
     }
+    setColorInversion(!colorInversion);
+    props.setIsSettingsModalOpen(!props.isOpen);
   };
+
+  useEffect(() => {
+    if (!props.isOpen) {
+      setColorInversion(false);
+    }
+  }, [props.isOpen]);
 
   return (
     <IconFramework
       icon={<IoIosSettings />}
       text="Settings"
       outline
+      inversion={colorInversion}
       onClick={() => handleClick()}
     />
   );
