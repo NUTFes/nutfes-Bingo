4d7ed2496c78c01669d4fc76e5125624ecf54158
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-08-26 16:18:48 +0900

    [fix] NumberCardLarge内の型定義を修正

--- view-user/src/components/common/NumberCardLarge/NumberCardLarge.tsx
+++ view-user/src/components/common/NumberCardLarge/NumberCardLarge.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import styles from "./NumberCardLarge.module.css";
+import { SubscribeListNumbersSubscription } from "@/type/graphql";
 
 interface NumberCardLargeProps {
-  bingoNumber: number;
+  bingoNumber: SubscribeListNumbersSubscription["numbers"][number];
 }
 
 const NumberCardLarge = (props: NumberCardLargeProps) => {
   return (
     <div className={styles.container}>
-      <div className={styles.number}>{props.bingoNumber}</div>
+      <div className={styles.number}>{props.bingoNumber.number}</div>
     </div>
   );
 };
