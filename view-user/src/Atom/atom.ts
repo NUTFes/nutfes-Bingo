b9b254520a414dda6ee6eae1f129e2c511066d6a
hikahana <22.h.hanada.nutfes@gmail.com>
2024-08-31 09:53:38 +0000

    atomの修正

--- view-user/src/Atom/atom.ts
+++ view-user/src/Atom/atom.ts
@@ -1,26 +1,24 @@
 import { atom } from "recoil";
 import type { GetListPrizesQuery } from "@/type/graphql";
-import { create } from "domain";
-import { updateContextWithCliFlags } from "@graphql-codegen/cli";
+
+const defaultPrize = {
+  id: 0,
+  nameJp: "",
+  nameEn: "",
+  isWon: false,
+  image: {
+    id: 0,
+    bucketName: "",
+    fileName: "",
+    fileType: "",
+    createdAt: "",
+    updatedAt: "",
+  },
+  createdAt: "",
+  updatedAt: "",
+};
 
 export const bingoPrizeState = atom<GetListPrizesQuery["prizes"]>({
   key: "bingoPrizeState",
-  default: [
-    {
-      id: 0,
-      nameJp: "",
-      nameEn: "",
-      isWon: false,
-      image: {
-        id: 0,
-        bucketName: "",
-        fileName: "",
-        fileType: "",
-        createdAt: "",
-        updatedAt: "",
-      },
-      createdAt: "",
-      updatedAt: "",
-    },
-  ],
+  default: [defaultPrize],
 });
