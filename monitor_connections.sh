9362450ab5d64cd7033325a9ac2ce0a9043f9d8b
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-09-02 03:39:19 +0900

    [feat] シェルスクリプトを追加

---
+++ monitor_connections.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+connection_count=0
+max_connection_count=0
+
+sudo docker compose -f docker-compose.prod.yml logs -f api | while read -r line
+do
+  if echo "$line" | grep -q "accepted"; then
+    ((connection_count++))
+    if (( connection_count > max_connection_count )); then
+      max_connection_count=$connection_count
+    fi
+  elif echo "$line" | grep -q "\"type\":\"closed\""; then
+    ((connection_count--))
+  fi
+  echo -ne "現在の同時接続数: $((connection_count / 2)), 最大同時接続数: $((max_connection_count / 2))\r"
+done
