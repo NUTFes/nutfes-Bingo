cedad0ba0a2222707fe073c10f357be289e79a08
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-09-02 15:07:57 +0900

    [fix] 出力結果のデザインとロジックの調整

--- monitor_connections.sh
+++ monitor_connections.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-connection_count=0
-max_connection_count=0
+current_connections=0
+max_connections=0
 
-sudo docker compose -f docker-compose.prod.yml logs -f api 2>/dev/null | while read -r line
-do
-  if echo "$line" | grep -q "accepted"; then
-    ((connection_count++))
-    if (( connection_count > max_connection_count )); then
-      max_connection_count=$connection_count
+HEADER_COLOR='\033[38;5;196m'
+DATA_COLOR='\033[38;5;222m'
+RESET_COLOR='\033[0m'
+
+sudo docker compose -f docker-compose.prod.yml logs -f api 2>/dev/null | while read -r line; do
+  if [[ "$line" == *"accepted"* ]]; then
+    ((current_connections++))
+    if ((current_connections > max_connections)); then
+      max_connections=$current_connections
     fi
-  elif echo "$line" | grep -q "closed"; then
-    ((connection_count--))
+  elif [[ "$line" == *"closed"* ]]; then
+    ((current_connections--))
   fi
-  echo -ne "現在の同時接続数: $connection_count, 最大同時接続数: $max_connection_count\r"
+
+  if [[ "$current_connections" != "$prevConnections" ]]; then
+    clear
+    prevConnections="$current_connections"
+
+    echo -e "${HEADER_COLOR}BINGO CONNECTION MONITOR${RESET_COLOR}\n"
+    echo -e "同時接続数: ${DATA_COLOR}$current_connections${RESET_COLOR}\n"
+    echo -e "最大同時接続数: ${DATA_COLOR}$max_connections${RESET_COLOR}\n"
+    echo -e "タイムスタンプ: ${DATA_COLOR}$(date +"%Y-%m-%d %H:%M:%S")${RESET_COLOR}\n"
+  fi
 done
