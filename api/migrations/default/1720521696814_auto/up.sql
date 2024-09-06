c75f40543075860b4f8efde469661bb021000726
hikahana <22.h.hanada.nutfes@gmail.com>
2024-09-06 06:53:02 +0000

    db-export

--- api/migrations/default/1720521696814_auto/up.sql
+++ api/migrations/default/1720521696814_auto/up.sql
@@ -160,6 +160,8 @@ ALTER TABLE ONLY public.prizes
 ALTER TABLE ONLY public.reach_logs
     ADD CONSTRAINT reach_log_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.stamp_triggers
+    ADD CONSTRAINT stamp_triggers_name_key UNIQUE (name);
+ALTER TABLE ONLY public.stamp_triggers
     ADD CONSTRAINT stamp_triggers_pkey PRIMARY KEY (id);
 CREATE TRIGGER set_public_stamp_triggers_updated_at BEFORE UPDATE ON public.stamp_triggers FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
 COMMENT ON TRIGGER set_public_stamp_triggers_updated_at ON public.stamp_triggers IS 'trigger to set value of column "updated_at" to current timestamp on row update';
