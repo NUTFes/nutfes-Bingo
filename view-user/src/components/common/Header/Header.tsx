c11cd6eb2893d3d5d0a3c682917ebf5cfc4d3845
Hayate Akiyama <24.h.akiyama.nutfes@gmail.com>
2024-09-08 21:23:15 +0900

    [fix]初期動作時にヘルプを開く

--- view-user/src/components/common/Header/Header.tsx
+++ view-user/src/components/common/Header/Header.tsx
@@ -3,14 +3,29 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import { IoHelpCircleOutline } from "react-icons/io5";
 import { Help } from "@/components";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Header = () => {
   const router = useRouter();
   const [isHelpOpen, setIsHelpOpen] = useState(false);
+
+  useEffect(() => {
+    const isHelpShown = localStorage.getItem("helpShown");
+
+    if (isHelpShown === null) {
+      setIsHelpOpen(true);
+      localStorage.setItem("helpShown", JSON.stringify(true));
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log("ヘルプ表示状態: ", isHelpOpen);
+  }, [isHelpOpen]);
+
   const goHelp = () => {
     setIsHelpOpen(true);
   };
+
   const closeHelp = () => {
     setIsHelpOpen(!isHelpOpen);
   };
