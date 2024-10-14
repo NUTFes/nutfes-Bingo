f39f7afd7ee09c41ac4f529c75c3ec69b2bbeba3
TkymHrt <23.h.takayama.nutfes@gmail.com>
2024-10-15 00:20:05 +0900

    [feat] svgr/webpackを追加

--- view-user/next.config.js
+++ view-user/next.config.js
@@ -25,6 +25,23 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgo: false,
+          },
+        },
+      ],
+    });
+    return config;
+  },
+  images: {
+    disableStaticImages: true,
+  },
 };
 
 module.exports = nextConfig;
