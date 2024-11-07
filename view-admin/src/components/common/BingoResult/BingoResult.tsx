ce49169d29b010d2c6cd04a8fdf3346cc966fac5
hikahana <22.h.hanada.nutfes@gmail.com>
2024-11-07 10:12:27 +0000

    不要なimportの削除

--- view-admin/src/components/common/BingoResult/BingoResult.tsx
+++ view-admin/src/components/common/BingoResult/BingoResult.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import styles from "./BingoResult.module.css";
 import { SubscribeListNumbersSubscription } from "@/type/graphql";
 
