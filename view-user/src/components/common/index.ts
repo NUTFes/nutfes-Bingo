0b6b4c33b67e61264b32d480a83416193719977d
Hayate Akiyama <24.h.akiyama.nutfes@gmail.com>
2024-12-04 18:08:04 +0900

    [fix]ヘルプカルーセル削除

--- view-user/src/components/common/index.ts
+++ view-user/src/components/common/index.ts
@@ -8,7 +8,6 @@ export { default as Modal } from "./Modal";
 export { default as Loading } from "./Loading";
 export { default as NavigationBar } from "./NavigationBar";
 export { default as ReactionStampModal } from "./ReactionStampModal";
-export { default as HelpCarousel } from "./HelpCarousel";
 export { default as ReachCount } from "./ReachCount";
 
 // Buttons
