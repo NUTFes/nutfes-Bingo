3bb9012ac26994f85de336a9c4871df0b9c1ef52
Hayate Akiyama <24.h.akiyama.nutfes@gmail.com>
2024-12-04 18:09:08 +0900

    [feat]intro.jsの実装

--- view-user/src/components/common/Header/Header.tsx
+++ view-user/src/components/common/Header/Header.tsx
@@ -1,41 +1,94 @@
+import introJs from "intro.js";
 import styles from "./Header.module.css";
 import { useRouter } from "next/router";
 import { IoHelpCircleOutline } from "react-icons/io5";
-import { HelpCarousel } from "@/components";
-import { useState, useEffect } from "react";
+import { useState, useEffect} from "react";
 import BingoLogo from "public/logo_bingo.svg";
+import { ja, en } from "@/locales";
 
 const Header = () => {
   const router = useRouter();
-  const [isOpenHelpCarousel, setIsOpenHelpCarousel] = useState(false);
+  const { locale } = useRouter();
+  const [] = useState<string>(locale || "ja");
+  const t = locale === "ja" ? ja : en;
 
+  const [isStartIntrojs, setIsStartIntrojs] = useState(false);
   useEffect(() => {
-    const isHelpShown = localStorage.getItem("isOpenHelpCarousel");
-
+    const isHelpShown = localStorage.getItem("isStartIntrojs");
     if (isHelpShown === null) {
-      setIsOpenHelpCarousel(true);
-      localStorage.setItem("isOpenHelpCarousel", JSON.stringify(true));
+      setIsStartIntrojs(true);
+      localStorage.setItem("isStartIntrojs", JSON.stringify(true));
+      startTour();
     }
   }, []);
 
-  const handleClick = () => {
-    setIsOpenHelpCarousel(true);
+  const startTour = () => {
+    const intro = introJs();
+    intro.onbeforechange(() => {
+      document.body.style.overflow = "hidden";
+      return true;
+    });
+    intro.oncomplete(() => {
+      document.body.style.overflow = "";
+    });
+
+    intro.setOptions({
+      steps: [
+        {
+          title: t.helpDescription.page1_title,
+          intro: t.helpDescription.page1_txt,
+          position: "floating",
+        },
+        {
+          title: t.helpDescription.page2_title,
+          intro: t.helpDescription.page2_txt,
+          element: "#PrizesIcon",
+          position: "bottom-middle-aligned",
+        },
+        {
+          title:t.helpDescription.page3_title,
+          intro: t.helpDescription.page3_txt,
+          element: "#ReactionsIcon",
+          position: "bottom-middle-aligned",
+        },
+        {
+          title:t.helpDescription.page4_title,
+          intro: t.helpDescription.page4_txt,
+          element: "#ReachIcon",
+          position: "bottom-middle-aligned",
+        },
+        {
+          title:t.helpDescription.page5_title,
+          intro: t.helpDescription.page5_txt,
+          element: "#SettingsIcon",
+          position: "bottom-middle-aligned",
+        },
+      ],
+      tooltipClass: styles.customTooltip,
+      scrollToElement: false,
+      hidePrev: true,
+      showBullets: true,
+      nextLabel: t.helpDescription.next,
+      prevLabel: t.helpDescription.back,
+      doneLabel: t.helpDescription.close,
+    });
+    intro.start();
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.main}>
-        <BingoLogo className={styles.logo} onClick={() => router.push("/")} />
-        <button className={styles.icon} onClick={handleClick}>
+        <BingoLogo
+          className={styles.logo}
+          onClick={() => router.push("/")}
+        />
+        <button
+          className={styles.icon}
+          onClick={startTour}
+        >
           <IoHelpCircleOutline />
         </button>
       </div>
-      {isOpenHelpCarousel && (
-        <HelpCarousel
-          isOpened={isOpenHelpCarousel}
-          setIsOpened={setIsOpenHelpCarousel}
-        />
-      )}
     </div>
   );
 };
